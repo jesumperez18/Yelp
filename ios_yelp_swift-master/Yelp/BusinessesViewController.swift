883868aeb0aa39b67f6dd17c049c83bbec3cf2f3
jesumperez18 <jesuperez@csumb.edu>
2018-02-18 20:42:16 -0800

    finished addinf search bar

---
+++ ios_yelp_swift-master/Yelp/BusinessesViewController.swift
@@ -0,0 +1,93 @@
+//
+//  BusinessesViewController.swift
+//  Yelp
+//
+//  Created by Timothy Lee on 4/23/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    var businesses: [Business]!
+    var filteredData: [Business]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight=120
+        
+        searchBar.delegate = self
+        
+        
+
+        
+        
+        
+        Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
+            
+            self.businesses = businesses
+            self.filteredData = businesses
+            self.tableView.reloadData()
+            if let businesses = businesses {
+                for business in businesses {
+                    print(business.name!)
+                    print(business.address!)
+                }
+            }
+            
+        }
+        )
+        
+        /* Example of Yelp search with more search options specified
+         Business.searchWithTerm("Restaurants", sort: .distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: Error!) -> Void in
+         self.businesses = businesses
+         
+         for business in businesses {
+         print(business.name!)
+         print(business.address!)
+         }
+         }
+         */
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if filteredData != nil {
+            return filteredData!.count
+        }else{
+            return 0
+        }
+    }
+    
+    // This method updates filteredData based on the text in the Search Box
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = searchText.isEmpty ? businesses : businesses.filter { (item: Business) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return item.name?.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+        
+        cell.business = filteredData[indexPath.row]
+        return cell
+    }
+    
+    
+}
